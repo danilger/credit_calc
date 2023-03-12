e726b9f7866e17cd7b2c605ccdb2535c47a8be06
danilger <81870573+danilger@users.noreply.github.com>
2023-03-12 12:13:10 +0500

    Create Calculator.js

---
+++ Calculator.js
@@ -0,0 +1,39 @@
+class Calculator {
+  #interestRate;
+  #maxDurationMonths;
+  #MinInitialPaymentPersent;
+
+  constructor(
+    persent = 0.2,
+    maxDurationMonths = 0,
+    MinInitialPaymentPersent = 0
+  ) {
+    this.#interestRate = persent / 12;
+    this.#maxDurationMonths = maxDurationMonths;
+    this.#MinInitialPaymentPersent = MinInitialPaymentPersent;
+  }
+
+  calculate(ammount, durationMonths = 5 * 12, initialPayment = 0) {
+    if (this.#MinInitialPaymentPersent > 0) {
+      initialPayment =
+        ammount * this.#MinInitialPaymentPersent > initialPayment
+          ? ammount * this.#MinInitialPaymentPersent
+          : initialPayment;
+    }
+
+    if (this.#maxDurationMonths > 0) {
+      durationMonths =
+        durationMonths > this.#maxDurationMonths
+          ? this.#maxDurationMonths
+          : durationMonths;
+    }
+
+    let annuityСoefficients =
+      (this.#interestRate * (1 + this.#interestRate) ** durationMonths) /
+      ((1 + this.#interestRate) ** durationMonths - 1);
+    let res = (ammount - initialPayment) * annuityСoefficients;
+    return Math.round(res);
+  }
+}
+
+export { Calculator };
